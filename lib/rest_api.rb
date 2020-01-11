ddd858bf5fdaaa7e5b1222dc7041191757b1e4c0
Mack Hasz <mack@hasz.email>
2020-01-10 17:21:57 -0800

    create base class

--- lib/rest_api.rb
+++ lib/rest_api.rb
@@ -1,50 +1,52 @@
 require "rest_api/version"
+require "rest-client"
 
 module RestApi
   class Error < StandardError; end
 
-  class << self
-    def get(path, headers={}, &block)
-      parse_response RestClient.get(build_url(path), headers.merge(base_headers), &block)
-    end
+  class Base
+    class << self
+      def get(path, headers={}, &block)
+        parse_response RestClient.get(build_url(path), headers.merge(base_headers), &block)
+      end
 
-    def post(path, payload, headers={}, &block)
-      parse_response RestClient.post(build_url(path), payload, headers.merge(base_headers), &block)
-    end
+      def post(path, payload, headers={}, &block)
+        parse_response RestClient.post(build_url(path), payload, headers.merge(base_headers), &block)
+      end
 
-    def patch(path, payload, headers={}, &block)
-      parse_response RestClient.patch(build_url(path), payload, headers.merge(base_headers), &block)
-    end
+      def patch(path, payload, headers={}, &block)
+        parse_response RestClient.patch(build_url(path), payload, headers.merge(base_headers), &block)
+      end
 
-    def put(path, payload, headers={}, &block)
-      parse_response RestClient.put(build_url(path), payload, headers.merge(base_headers), &block)
-    end
+      def put(path, payload, headers={}, &block)
+        parse_response RestClient.put(build_url(path), payload, headers.merge(base_headers), &block)
+      end
 
-    def delete(path, headers={}, &block)
-      parse_response RestClient.delete(build_url(path), headers.merge(base_headers), &block)
-    end
+      def delete(path, headers={}, &block)
+        parse_response RestClient.delete(build_url(path), headers.merge(base_headers), &block)
+      end
 
-    def head(path, headers={}, &block)
-      parse_response RestClient.head(build_url(path), headers.merge(base_headers), &block)
-    end
+      def head(path, headers={}, &block)
+        parse_response RestClient.head(build_url(path), headers.merge(base_headers), &block)
+      end
 
-    def options(path, headers={}, &block)
-      parse_response RestClient.options(build_url(path), headers.merge(base_headers), &block)
-    end
+      def options(path, headers={}, &block)
+        parse_response RestClient.options(build_url(path), headers.merge(base_headers), &block)
+      end
 
-    private
+      private
 
-    def build_url(path)
-      "#{const_get 'BASE_URL'}/#{path}"
-    end
+      def build_url(path)
+        "#{const_get 'BASE_URL'}/#{path}"
+      end
 
-    def parse_response(response)
-      RecursiveOpenStruct.new(JSON.parse(response))
-    end
+      def parse_response(response)
+        RecursiveOpenStruct.new(JSON.parse(response))
+      end
 
-    def base_headers
-      {}
+      def base_headers
+        {}
+      end
     end
   end
-
 end
